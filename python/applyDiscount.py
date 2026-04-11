dc498ffbfcd1b3ac364a34d4639f74a894e2e243
PuzzledFELIPE <fesou70@gmail.com>
2026-04-11 19:57:08 -0300

    python challenge corrected

--- python/applyDiscount.py
+++ python/applyDiscount.py
@@ -1,12 +1,18 @@
 def applyDiscount(price, discount):
-    price = float(input("Enter the product price: "))
-    discount = float(input("Enter the discount percentage: "))
 
-    if price != int or float or discount != int or float:
-        return "The price and discount should be a number"
-    if discount not in range(0, 100):
+    if not isinstance(price, (int, float)):
+        return "The price should be a number" 
+    elif not isinstance(discount, (int, float)):
+        return "The discount should be a number"
+    elif discount not in range(0, 100):
         return "The discount should be between 0 and 100"
-    if price > 0:
-        return price - (price * discount / 100)
+    elif price > 0:
+        finalPrice = price - (price * discount / 100)
+        return finalPrice
     else:        
         return "The price should be greater than 0"
+    
+price = float(input("Enter the product price: "))
+discount = float(input("Enter the discount percentage: "))
+print(applyDiscount(price, discount))
+
