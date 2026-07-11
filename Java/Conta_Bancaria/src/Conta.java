b91f2ffdecef3ffca01d34eb76ccd274c67617a5
PuzzledFELIPE <fesou70@gmail.com>
2026-07-11 17:19:15 -0300

    Changes in Bank account excercise

--- Java/Conta_Bancaria/src/Conta.java
+++ Java/Conta_Bancaria/src/Conta.java
@@ -1,31 +1,74 @@
+import java.util.Scanner;
+
 public class Conta {
+
     private float saldo;
 
     private float chequeEspecial;
 
+    private float valorDeposito;
+
+    private float valorSaque;
+
+    private boolean primeiroDeposito;
+
+    private float valorBoleto;
     //não esquecer do boleto
 
     public float getSaldo() {
         return saldo;
     }
 
-    public void setSaldo(float saldo) {
-        this.saldo = saldo;
-    }
+    public float getValorDeposito() { return valorDeposito; }
 
     public float getChequeEspecial() {
         return chequeEspecial;
     }
 
-    public void setChequeEspecial(float chequeEspecial) {
-        this.chequeEspecial = chequeEspecial;
+    public void definirValorCheque(){
+        if(primeiroDeposito && valorDeposito<=500){
+            chequeEspecial = 50;
+        } else if(primeiroDeposito && valorDeposito>500) {
+            chequeEspecial = 0.5F*valorDeposito;
+        }
     }
 
-    public void definirValorCheque(){//vai de acordo com o valor do depósito na criação da conta
+    public void isPrimeiroDeposito(){
+        primeiroDeposito = Main.contador == 0;
     }
 
-    public void deposito(){
-        
+    public void deposito() {
+        var scanner = new Scanner(System.in);
+        float valorDeposito = 0;
+        if (!primeiroDeposito) {
+            System.out.println("Digite o valor do depósito: ");
+            valorDeposito = scanner.nextFloat();
+            System.out.println("Depositando...");
+            System.out.println("Depósito feito com sucesso, o saldo atual é de R$" + (saldo + valorDeposito));
+        } else {
+            System.out.println("Digite o valor do seu primeiro depósito: ");
+            valorDeposito = scanner.nextFloat();
+            //colocar o print do primeiro depósito na main
+        }
     }
 
+    public void saque(){
+        var scanner = new Scanner(System.in);
+        float valorSaque = 0;
+        System.out.println("Digite o valor do Saque: ");
+        valorSaque = scanner.nextFloat();
+        System.out.println("Realizando saque...");
+        System.out.println("Saque feito com sucesso, o saldo atual é de R$" + (saldo - valorSaque));
+    }
+
+    public void pagarBoleto(){
+        var scanner = new Scanner(System.in);
+        System.out.println("Digite o valor do boleto a pagar: ");
+        valorBoleto = scanner.nextFloat();
+
+        if(valorBoleto>saldo){
+            System.out.println();//mensagem de saldo insuficiente e indicar fazer depósito;
+        }
+
+    }
 }
--- Java/Conta_Bancaria/src/Main.java
+++ Java/Conta_Bancaria/src/Main.java
@@ -1,6 +1,11 @@
 public class Main(){
+
+    public static int contador = -1;
+
     public static void main(String[] args){
+
+
         //funçaõ de criação da conta tem que ser aqui
-        //criar o saldo visualizado aqui
+        //criar funções de consulta saldo e cheque aqui
     }
 }
