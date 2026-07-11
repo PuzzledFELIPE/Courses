d85030d4946c831dfe7e6ea13b3bff1683506c62
PuzzledFELIPE <fesou70@gmail.com>
2026-07-11 01:03:25 -0300

    bank acount excercise in java

---
+++ Java/Conta_Bancaria/src/Conta.java
@@ -0,0 +1,31 @@
+public class Conta {
+    private float saldo;
+
+    private float chequeEspecial;
+
+    //não esquecer do boleto
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public float getChequeEspecial() {
+        return chequeEspecial;
+    }
+
+    public void setChequeEspecial(float chequeEspecial) {
+        this.chequeEspecial = chequeEspecial;
+    }
+
+    public void definirValorCheque(){//vai de acordo com o valor do depósito na criação da conta
+    }
+
+    public void deposito(){
+        
+    }
+
+}
---
+++ Java/Conta_Bancaria/src/Main.java
@@ -0,0 +1,6 @@
+public class Main(){
+    public static void main(String[] args){
+        //funçaõ de criação da conta tem que ser aqui
+        //criar o saldo visualizado aqui
+    }
+}
