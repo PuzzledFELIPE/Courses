29bb4c07434fa09307e5fc72a93efeb2884e14ac
PuzzledFELIPE <fesou70@gmail.com>
2026-07-17 16:30:04 -0300

    World  clocks excercise

---
+++ Java/World_Clocks_Excercise/src/Main.java
@@ -0,0 +1,18 @@
+import Clocks.BRLClock;
+import Clocks.Clock;
+import Clocks.USClock;
+
+public class Main {
+
+    public static void main(String[] args){
+        Clock brlclock = new BRLClock();
+        brlclock.setSecond(0);
+        brlclock.setMinute(0);
+        brlclock.setHour(25);
+
+        System.out.println(brlclock.getTime());
+
+        System.out.println(new USClock().convert(brlclock).getTime());
+
+    }
+}
