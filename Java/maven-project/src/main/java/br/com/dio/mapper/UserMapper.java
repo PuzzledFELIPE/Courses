a5eefec391bfe5b665d39890b811603d6320fb99
PuzzledFELIPE <fesou70@gmail.com>
2026-07-30 18:17:18 -0300

    deleted function mapping

--- Java/maven-project/src/main/java/br/com/dio/mapper/UserMapper.java
+++ Java/maven-project/src/main/java/br/com/dio/mapper/UserMapper.java
@@ -7,13 +7,4 @@ import org.mapstruct.Mapping;
 
 @Mapper
 public interface UserMapper {
-
-    @Mapping(target = "code", source = "dto");
-    @Mapping(target = "userName", source = "name");
-    UserModel toModel(final UserDTO dto);
-
-    @Mapping(target = "code", source = "dto");
-    @Mapping(target = "name", source = "userName");
-    UserDTO toDTO(final UserModel dto);
-
 }
